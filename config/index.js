4eecb6121ac4e88012bde60f9d294485e1511037
cynthia101488 <cynthia101488@gmail.com>
2021-11-12 23:54:44 +0800

    20211112 update

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/food__page__vue/',
 
     /**
      * Source Maps
